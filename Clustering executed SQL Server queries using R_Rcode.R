a3d24e85010ddf14b7f5a01194f0df1e0824b90b
tomaztk <tomaz.kastrun@gmail.com>
2017-01-08 11:04:03 +0100

    Add files via upload

--- Clustering executed SQL Server queries using R_Rcode.R
+++ Clustering executed SQL Server queries using R_Rcode.R
@@ -4,7 +4,7 @@ myconn <-odbcDriverConnect("driver={SQL Server};Server=SICN-KASTRUN;database=Wid
 
 
 query.data <- sqlQuery(myconn, "
-                      SELECT 
+                     SELECT 
 					             [total_logical_io]
                       ,[avg_logical_reads]
                       ,[avg_phys_reads]
@@ -13,7 +13,13 @@ query.data <- sqlQuery(myconn, "
                       ,[total_elapsed_time]
                       ,total_dop
                       ,[text]
-                      ,LEFT([text],35) AS label_graph 
+                      ,CASE WHEN LEFT([text],70) LIKE '%AbtQry%' THEN 'AbtQry'
+          							 WHEN LEFT([text],70) LIKE '%OrdQry%' THEN 'OrdQry'
+          							 WHEN LEFT([text],70) LIKE '%PrsQry%' THEN 'PrsQry'
+          							 WHEN LEFT([text],70) LIKE '%SalQry%' THEN 'SalQry'
+          							 WHEN LEFT([text],70) LIKE '%PurQry%' THEN 'PurQry'
+          							 WHEN LEFT([text],70) LIKE '%@BatchID%' THEN 'System'
+          						ELSE 'Others' END AS label_graph 
                       FROM query_stats_LOG_2")
 
 close(myconn) 
@@ -21,13 +27,14 @@ close(myconn)
 
 library(cluster)
 
-qd <- query.data[,c(1,2,3,5,6)]
+#qd <- query.data[,c(1,2,3,5,6)]
+qd <- query.data[,c(1,2,6)]
 
 ## hierarchical clustering
-qd.use <- query.data[,c(1,2,3,5,6)]
+qd.use <- query.data[,c(1,2,6)]
 medians <- apply(qd.use,2,median)
 mads <- apply(qd.use,2,mad)
-qd.use <- scale(qd.use,center=medians,scale=mads)
+qd.use <- scale(qd.use,center=medians) #,scale=mads)
 
 #calculate distances
 query.dist <- dist(qd.use)
@@ -36,7 +43,12 @@ query.dist <- dist(qd.use)
 query.hclust <- hclust(query.dist)
 
 # plotting solution
-plot(query.hclust,labels=query.data$label_graph,main='Default from hclust')
+op = par(bg = "lightblue")
+plot(query.hclust,labels=query.data$label_graph,main='Query Hierarchical Clustering', ylab = 'Distance', xlab = ' ', hang = -1, sub = "" )
+# in addition to circle queries within cluster
+rect.hclust(query.hclust, k=3, border="DarkRed")		
+
+
 
 # we get 3 clusters
 groups.3 = cutree(query.hclust,3)
@@ -50,7 +62,5 @@ query.pam <- pam(query.dist,3)
 table(groups.3,query.pam$clustering)
 
 # both solutions seem to agree on queries clusters
-
-
 plot(query.pam)
 
